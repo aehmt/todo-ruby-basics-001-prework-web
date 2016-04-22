9e9704e17bd8dbf2557ca8e1b3be843d3586ade4
Ahmet Calis <calisahmet@gmail.com>
2016-04-22 14:17:04 -0400

    todo ruby basics 1

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
 def division(num1, num2)
- 
+  division = num1/num2
+  return division
 end
 
-def assign_variable(value)
-  
+def assign_variable(name = "Bob")
+  value = name
 end
 
-def argue
-  
+def argue(phrase)
+  phrase = "I'm right and you are wrong!"
 end
 
-def greeting
-
+def greeting(intro, name)
+  puts intro + name
 end
 
-def return_a_value
-
+def return_a_value(value = "Nice")
+  value
 end
 
-def last_evaluated_value
-
+def last_evaluated_value(value_1 = "expert")
+  value_1
 end
 
-def pizza_party
-
+def pizza_party(ingredient = "cheese")
+  return ingredient
 end
